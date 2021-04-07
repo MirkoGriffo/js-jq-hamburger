a66acd6612fed1a0fc62a964ab07462954572a6b
MirkoGriffo <mirkogriffo@yahoo.it>
2021-04-07 16:41:28 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+var apri = $(".fa-bars");
+var chiudi = $(".fa-times");
+var hamburgerMenu = $(".hamburger-menu");
+
+
+apri.click(function () {
+    hamburgerMenu.show();
+});
+
+
+chiudi.click(function () {
+    hamburgerMenu.hide();
+});
